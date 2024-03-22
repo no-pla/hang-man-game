c9e28af52d75fb8a5a20eb3149639ba70358be73
no-pla <songjihyun323@gmail.com>
2024-03-22 15:46:54 +0900

    fix: separate authOption to file to resolve build failure issue

---
+++ share/authOptions.ts
@@ -0,0 +1,71 @@
+import { PrismaClient } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { label: "email", type: "text", placeholder: "email" },
+        password: { label: "password ", type: "password" },
+      },
+      async authorize(credentials) {
+        // authorize => jwt callback => session callback 순서로 실행된다.
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("빈 필드를 제출했습니다.");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user) {
+          throw new Error("이메일 혹은 비밀번호를 다시 확인해 주세요.");
+        }
+
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!passwordsMatch) {
+          throw new Error("이메일 혹은 비밀번호를 다시 확인해 주세요.");
+        }
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (user) {
+        return {
+          id: user.id,
+          picture: user.image,
+          ...token,
+        };
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      return {
+        ...session,
+        user: {
+          id: token.id,
+          ...session.user,
+        },
+      };
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET as string,
+};
